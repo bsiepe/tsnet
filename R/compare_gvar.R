87c80756c16d657d02d6feeb5f79b8e60e139663
Bjoern Siepe <bjoernsiepe@gmail.com>
2023-07-11 09:14:47 +0200

    add plotting method

--- R/compare_gvar.R
+++ R/compare_gvar.R
@@ -20,7 +20,8 @@
 #' The distance metric to use. Should be one of "frob" (Frobenius norm), "maxdiff" (maximum  difference), or "l1" (L1 norm) (default: "frob").
 #' The use of the Frobenius norm is recommended.
 #' @param return_all
-#' Logical indicating whether to return all distributions (default: FALSE)
+#' Logical indicating whether to return all distributions (default: FALSE).
+#' Has to be set to TRUE for plotting the results.
 #' @return A list containing the results of the comparison. The list includes:
 #'  \itemize{
 #'   \item{sig_beta}{Binary decision on whether there is a significant difference between the temporal networks of A and B}
@@ -199,6 +200,86 @@ compare_gvar <- function(fit_a,
   }
 
 
-
+  class(l_res) <- "compare_gvar"
   return(l_res)
 }
+
+# Plotting method
+plot.compare_gvar <- function(x,
+                              name_a = NULL,   # set name for a manually
+                              name_b = NULL,   # set name for b manually
+                              ...){
+
+  # Input check
+  if(is.null(x$null)){
+    stop("Reference distributions of compare_gvar must be saved using the
+         argument 'return_all'=TRUE ")
+  }
+
+  # sysfonts::font_add_google("News Cycle", "news")
+  # # use showtext
+  # showtext::showtext_auto()
+
+  # Exchange names
+  if(!is.null(name_a)){
+    name_a <- as.character(name_a)
+    x$res_beta$mod <- gsub("mod_a", name_a, x$res_beta$mod)
+    x$res_pcor$mod <- gsub("mod_a", name_a, x$res_pcor$mod)
+  }
+  if(!is.null(name_b)){
+    name_b <- as.character(name_b)
+    x$res_beta$mod <- gsub("mod_b", name_b, x$res_beta$mod)
+    x$res_pcor$mod <- gsub("mod_b", name_b, x$res_pcor$mod)
+  }
+
+
+
+  # Plotting
+  plt_beta <- ggplot(x$res_beta,
+                     aes(x = .data$null,
+                         fill = .data$mod))+
+    geom_density(alpha = .7)+
+    theme_classic()+
+    ggokabeito::scale_fill_okabe_ito()+
+    geom_vline(aes(xintercept = x$emp_beta),
+               col = "red", lty = 1, linewidth = .75)+
+    scale_y_continuous(expand = c(0,0))+
+    labs(title = "Temporal",
+         y = "",
+         x = "Norm Value")+
+    # theme_compare()+
+    theme(axis.ticks.y = element_blank(),
+          axis.text.y = element_blank(),
+          legend.position = "right")
+
+
+  plt_pcor <- ggplot(x$res_pcor,
+                     aes(x = .data$null,
+                         fill = .data$mod))+
+    geom_density(alpha = .7)+
+    theme_classic()+
+    ggokabeito::scale_fill_okabe_ito()+
+    geom_vline(aes(xintercept = x$emp_pcor),
+               col = "red", lty = 1, linewidth = .75)+
+    scale_y_continuous(expand = c(0,0))+
+    labs(title = "Contemporaneous",
+         y = "",
+         x = "Norm Value")+
+    # theme_compare()+
+    theme(axis.ticks.y = element_blank(),
+          axis.text.y = element_blank(),
+          legend.position = "right")
+
+  leg <- get_legend(plt_beta)
+
+  # Plot
+  plt_tmp <- cowplot::plot_grid(plt_beta + theme(legend.position = "none"),
+                                plt_pcor + theme(legend.position = "none"))
+
+  # Add legend
+  plt <- plot_grid(plt_tmp, leg, rel_widths = c(3, .4))
+  plt
+
+}
+
+
