442d0b769b44e188c92b0799a0c0b35cc2f326e7
Bjoern Siepe <bjoernsiepe@gmail.com>
2024-01-09 10:12:00 +0100

    fix check_eigen naming issue, add documentation

--- R/check_eigen.R
+++ R/check_eigen.R
@@ -1,6 +1,6 @@
 #' Check Eigenvalues of BGGM Object
 #'
-#' This function checks the eigenvalues of Beta to assure that the model is stationary. It uses the same check as the `graphicalVAR` package. 
+#' This function checks the eigenvalues of Beta to assure that the model is stationary. It uses the same check as the `graphicalVAR` package.
 #'
 #' @param fitobj A BGGM object.
 #' @return A list containing the eigenvalues and a verbal summary of the results.
@@ -9,17 +9,17 @@ check_eigen <- function(fitobj){
   beta_mu <- fitobj$beta_mu
   # Calculate eigenvalues
   eigen_beta_mu <- eigen(beta_mu)$values
-  
-  # check if the sum of the squares of the real and imaginary parts of the eigenvalues of the coefficient matrix is less than 1 
+
+  # check if the sum of the squares of the real and imaginary parts of the eigenvalues of the coefficient matrix is less than 1
   # If it is, it implies that the VAR model is stable
-  check_beta_mu <- all(Re(eigen_beta)^2 + Im(eigen_beta)^2 <1)
+  check_beta_mu <- all(Re(eigen_beta_mu)^2 + Im(eigen_beta_mu)^2 <1)
   if(isTRUE(check_beta_mu)){
     response <- "stable."
   }
   else {
      response <- "not stable. It is recommended to refit the model and to inspect your data."
   }
-  
+
   # Return verbal summary
   print(paste0("The VAR coefficient matrix of the input model is ", response))
 
