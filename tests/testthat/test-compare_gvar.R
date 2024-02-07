07312c33f13963737e866bf8c993913632bb6bd5
Bjoern Siepe <bjoernsiepe@gmail.com>
2024-02-07 16:25:39 +0100

    expand previous tests

--- tests/testthat/test-compare_gvar.R
+++ tests/testthat/test-compare_gvar.R
@@ -4,14 +4,186 @@ test_that("input check for fit objects", {
   expect_error(compare_gvar(fit_a, fit_b))
 })
 
-test_that("compare_gvar works for var_estimate objects", {
-  data("ts_data")
-  data1 <- subset(ts_data, id == "ID1")
-  data1 <- subset(data1, select = -c(id))
-  fit1 <- BGGM::var_estimate(data1)
-  data2 <- subset(ts_data, id == "ID2")
-  data2 <- subset(data2, select = -c(id))
-  fit2 <- BGGM::var_estimate(data2)
+test_that("compare_gvar fails if one model is incorrect", {
+  data(fit_data)
+  expect_error(compare_gvar(fit_data[[1]], matrix(rnorm(10)), n_draws = 100))
+})
 
-  expect_silent(compare_gvar(fit1, fit2))
+
+test_that("compare_gvar works for two fits", {
+  data(fit_data)
+  test_res <-
+    compare_gvar(fit_data[[1]], fit_data[[2]], n_draws = 100)
+  expect_s3_class(test_res, "compare_gvar")
+  expect_no_error(test_res)
+
+})
+
+
+
+test_that("compare_gvar returns zero difference for identical models", {
+  data(fit_data)
+  test_res <-
+    compare_gvar(fit_data[[1]], fit_data[[1]], n_draws = 100)
+  expect_equal(test_res$emp_beta, 0)
+  expect_equal(test_res$emp_pcor, 0)
+})
+
+
+test_that("compare_gvar returns non-zero difference for different models",
+          {
+            data(fit_data)
+            test_res <-
+              compare_gvar(fit_data[[1]], fit_data[[2]], n_draws = 100)
+            expect_gt(test_res$emp_beta, 0)
+            expect_gt(test_res$emp_pcor, 0)
+          })
+
+test_that("compare_gvar fails for incorrect cutoff", {
+  data(fit_data)
+  expect_error(compare_gvar(fit_data[[1]],
+                            fit_data[[2]],
+                            cutoff = 101,
+                            n_draws = 100))
+  expect_error(compare_gvar(
+    fit_data[[1]],
+    fit_data[[2]],
+    cutoff = -0.1,
+    n_draws = 100
+  ))
+})
+
+test_that("compare_gvar warns for low cutoff", {
+  data(fit_data)
+  expect_warning(compare_gvar(fit_data[[1]],
+                              fit_data[[2]],
+                              cutoff = 0.1,
+                              n_draws = 100))
+})
+
+test_that("compare_gvar fails for incorrect indices", {
+  data(fit_data)
+  expect_error(compare_gvar(fit_data[[1]],
+                            fit_data[[2]],
+                            n_draws = 100,
+                            indices = 1:10))
+  expect_error(compare_gvar(fit_data[[1]],
+                            fit_data[[2]],
+                            n_draws = 100,
+                            indices = list(theta = 1:10)))
+  expect_error(compare_gvar(fit_data[[1]],
+                            fit_data[[2]],
+                            n_draws = 100,
+                            indices = list(theta = 1:10, beta = 1:10)))
+  expect_error(compare_gvar(fit_data[[1]],
+                            fit_data[[2]],
+                            n_draws = 100,
+                            indices = list(beta = "wrong", pcor = 1:10)))
+  expect_error(compare_gvar(fit_data[[1]],
+                            fit_data[[2]],
+                            n_draws = 100,
+                            indices = list(beta = 1:10, pcor = "wrong")))
+})
+
+test_that("compare_gvar breaks for wrong test arguments", {
+  data(fit_data)
+  expect_error(compare_gvar(fit_data[[1]],
+                            fit_data[[2]],
+                            n_draws = 100,
+                            comp = "wrong"))
+  expect_error(compare_gvar(fit_data[[1]],
+                            fit_data[[2]],
+                            n_draws = 100,
+                            dec_rule = "wrong"))
+})
+
+
+test_that("compare_gvar works for correct indices", {
+  data(fit_data)
+  test_res <-
+    compare_gvar(fit_data[[1]],
+                 fit_data[[2]],
+                 n_draws = 100,
+                 indices = list(beta = 1:2, pcor = 1:2))
+  expect_s3_class(test_res, "compare_gvar")
+  expect_no_error(test_res)
+})
+
+test_that("compare_gvar works with different comps", {
+  data(fit_data)
+  test_res <-
+    compare_gvar(fit_data[[1]],
+                 fit_data[[2]],
+                 n_draws = 100,
+                 comp = "l1")
+  expect_s3_class(test_res, "compare_gvar")
+  expect_no_error(test_res)
+
+  test_res <-
+    compare_gvar(fit_data[[1]],
+                 fit_data[[2]],
+                 n_draws = 100,
+                 comp = "maxdiff")
+  expect_s3_class(test_res, "compare_gvar")
+  expect_no_error(test_res)
+})
+
+test_that("compare_gvar works with different dec_rule", {
+  data(fit_data)
+  test_res <-
+    compare_gvar(fit_data[[1]],
+                 fit_data[[2]],
+                 n_draws = 100,
+                 dec_rule = "comb")
+  expect_s3_class(test_res, "compare_gvar")
+  expect_no_error(test_res)
+
+})
+
+
+test_that("compare_gvar return_all works",{
+  data(fit_data)
+  test_res <-
+    compare_gvar(fit_data[[1]],
+                 fit_data[[2]],
+                 n_draws = 100,
+                 return_all = TRUE)
+  expect_s3_class(test_res, "compare_gvar")
+  expect_no_error(test_res)
+  expect_true(all(c("res_beta", "res_pcor") %in% names(test_res)))
+})
+
+test_that("compare_gvar converts stanfit",{
+  data(ts_data)
+  example_data <- ts_data[1:100,1:3]
+  fit <- stan_gvar(example_data, n_chains = 1)
+  test_res <-
+    compare_gvar(fit,
+                 fit,
+                 n_draws = 100)
+  expect_s3_class(test_res, "compare_gvar")
+  expect_no_error(test_res)
+})
+
+test_that("compare_gvar returns expected distances",{
+  data(fit_data)
+  test_res <-
+    compare_gvar(fit_data[[1]],
+                 fit_data[[2]],
+                 n_draws = 100,
+                 comp = "frob")
+
+  # calculate distances between posterior means
+  beta_diff <- norm(fit_data[[1]]$beta_mu - fit_data[[2]]$beta_mu,
+                    type = "F")
+  ut <- function(x) {
+      matrix(x[upper.tri(x, diag = FALSE)])
+  }
+  pcor_diff <- norm(ut(fit_data[[1]]$pcor_mu) - ut(fit_data[[2]]$pcor_mu),
+                    type = "F")
+
+  expect_equal(test_res$emp_beta, beta_diff)
+  expect_equal(test_res$emp_pcor, pcor_diff)
+
+
 })
--- tests/testthat/test-post_distance_within.R
+++ tests/testthat/test-post_distance_within.R
@@ -1,17 +1,113 @@
-test_that("post_distance_within calculates distances correctly for empirical models", {
-  data("ts_data")
-  data1 <- subset(ts_data, id == "ID1")
-  data1 <- subset(data1, select = -c(id))
-  fitobj <- BGGM::var_estimate(data1)
-  comp <- "frob"
-  draws <- 1000
+test_that("post_distance_within works with intended input",{
+  data(fit_data)
+  results <- post_distance_within(fit_data[[1]],
+                                  pred = FALSE,
+                                  n_draws = 100,
+                                  comp = "frob")
 
-  result <- post_distance_within(fitobj, comp, pred = FALSE, draws = draws)
+  expect_type(results, "list")
+  expect_true(all(c("comp", "mod_one", "mod_two", "beta", "pcor") %in% colnames(results)))
 
-  # Check if the result has the correct number of rows
-  expect_equal(nrow(result), draws)
+})
 
-  # Check if the calculated distances are numeric
-  expect_true(all(is.na(result$beta) | is.numeric(result$beta)))
-  expect_true(all(is.na(result$pcor) | is.numeric(result$pcor)))
+test_that("post_distance_within works with sequential sampling",{
+  data(fit_data)
+  results <- post_distance_within(fit_data[[1]],
+                                  pred = FALSE,
+                                  n_draws = 100,
+                                  comp = "frob",
+                                  sampling_method = "sequential")
+
+  expect_type(results, "list")
+  expect_true(all(c("comp", "mod_one", "mod_two", "beta", "pcor") %in% colnames(results)))
+
+})
+
+test_that("post_distance_within works with differenct comps", {
+  data(fit_data)
+  results <- post_distance_within(fit_data[[1]],
+                                  pred = FALSE,
+                                  n_draws = 100,
+                                  comp = "l1")
+
+  expect_type(results, "list")
+  expect_true(all(c("comp", "mod_one", "mod_two", "beta", "pcor") %in% colnames(results)))
+
+  results <- post_distance_within(fit_data[[1]],
+                                  pred = FALSE,
+                                  n_draws = 100,
+                                  comp = "maxdiff")
+
+  expect_type(results, "list")
+  expect_true(all(c("comp", "mod_one", "mod_two", "beta", "pcor") %in% colnames(results)))
+
+})
+
+test_that("post_distance_within breaks when input is not a list",{
+  expect_error(post_distance_within(matrix(rnorm(100)),
+                                    pred = FALSE,
+                                    n_draws = 100,
+                                    comp = "frob"))
+})
+
+test_that("post_distance_within accepts indices",{
+  data(fit_data)
+  results <- post_distance_within(fit_data[[1]],
+                                  pred = FALSE,
+                                  n_draws = 100,
+                                  comp = "frob",
+                                  indices = list(beta = 1:2, pcor = 3:4))
+
+  expect_type(results, "list")
+  expect_true(all(c("comp", "mod_one", "mod_two", "beta", "pcor") %in% colnames(results)))
+
+})
+
+test_that("post_distance_within works for posterior predictive lists",{
+  data(fit_data)
+  # Create fake posterior predictive draws
+  pp_list <- list()
+  pp_list$fit <- lapply(1:100, function(x) fit_data[[1]])
+
+  results <- post_distance_within(pp_list,
+                                  pred = TRUE,
+                                  n_draws = 50,
+                                  comp = "frob")
+
+
+  expect_type(results, "list")
+  expect_true(all(c("comp", "mod_one", "mod_two", "beta", "pcor") %in% colnames(results)))
+
+})
+
+test_that("post_distance_within works for posterior predictive lists with indices",{
+  data(fit_data)
+  # Create fake posterior predictive draws
+  pp_list <- list()
+  pp_list$fit <- lapply(1:100, function(x) fit_data[[1]])
+
+  results <- post_distance_within(pp_list,
+                                  pred = TRUE,
+                                  n_draws = 50,
+                                  comp = "frob",
+                                  indices = list(beta = 1:2, pcor = 3:4))
+
+  expect_type(results, "list")
+  expect_true(all(c("comp", "mod_one", "mod_two", "beta", "pcor") %in% colnames(results)))
+
+})
+
+test_that("post_distance_within for posterior predictive lists breaks when one model is not a list",{
+  data(fit_data)
+  # Create fake posterior predictive draws
+  pp_list <- list()
+  pp_list$fit <- lapply(1:100, function(x) fit_data[[1]])
+  pp_list$fit[[1]] <- matrix(rnorm(100))
+
+  expect_error(post_distance_within(pp_list,
+                                    pred = TRUE,
+                                    n_draws = 50,
+                                    comp = "frob",
+                                    indices = list(beta = 1:2, pcor = 3:5)))
+
 })
